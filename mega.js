511d98d66d81d14df82be3622744ff453829a840
CHALAH374 <sethustatus416@gmail.com>
2025-08-18 18:10:08 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ const mega = require("megajs");
 const fs = require("fs");
 
 const auth = {
-  email: "mahiyabotz@gmail.com",
-  password: "mutgmw@0624",
+  email: "sethustatus416@gmail.com",
+  password: "CHALAH@1229@",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
